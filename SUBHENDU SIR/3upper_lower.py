79c46b12f7c46da5c4e005f045b99e8e13700b6b
Soumita Das <soumitad991@gmail.com>
2024-11-06 09:52:18 +0530

    PYTHON COLLEGE

--- SUBHENDU SIR/3upper_lower.py
+++ SUBHENDU SIR/3upper_lower.py
@@ -1,3 +1,4 @@
+#Write a python program to enter any character if the character is in lowercase, convert it into uppercase and if is in uppercase convert is into lowercase.
 char=input("Enter a character: ")
 if (char.isalpha and len(char)==1):
     if char.islower():
